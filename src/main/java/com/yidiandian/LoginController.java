436e6d79273d963df0cc12518d1bdc74771aec5f
wb00001086 <abcd1234>
2019-05-30 17:08:24 +0800

    数据库表导出数据结构到word 文档

---
+++ src/main/java/com/yidiandian/LoginController.java
@@ -0,0 +1,22 @@
+package com.yidiandian;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable{
+
+	@FXML
+	private ImageView img;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Image image = new Image("/head_3.jpg");
+		img.setImage(image);
+		
+	}
+}
